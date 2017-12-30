03193db5b3e45bfa6151146066ccfa02313370fc
Homer <homerquan@gmail.com>
2017-12-29 19:07:54 -0500

    a regular commit

---
+++ src/config/enviroment/development.js
@@ -0,0 +1,27 @@
+'use strict';
+
+// Development specific configuration
+// ==================================
+module.exports = {
+
+	logLvl: 'debug',
+
+	hashSalt: 'ilikeconvospot',
+
+	amqpConn: 'amqp://guest:guest@localhost:5672/seneca',
+
+	// Server port
+	port: process.env.PORT || 8002,
+
+	// Server IP
+	ip: process.env.IP || '0.0.0.0',
+
+	// API Gateway
+	api: {
+		// API URL to be used in the client-side code
+		clientUrl: process.env.API_CLIENT_URL || `http://localhost:${process.env.PORT || 8003}`,
+		// API URL to be used in the server-side code
+		serverUrl: process.env.API_SERVER_URL || `http://localhost:${process.env.PORT || 8003}`,
+	},
+
+};
---
+++ src/config/index.js
@@ -0,0 +1,13 @@
+/* eslint-disable max-len */
+
+let config = {};
+if (process.env.BROWSER) {
+  // for client side code, only load shared config
+  config = require('./enviroment/shared');
+} else {
+  const envConfig = require('./enviroment');
+  // Using RC to read settings in /etc/appnamerc
+  config = require('rc')(process.env.npm_package_name || 'convospot-console', envConfig);
+}
+
+module.exports = config;
