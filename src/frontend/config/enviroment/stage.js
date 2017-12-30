f86015d83792ba8cd26c8f584c465d81db9ffa4e
Homer <homerquan@gmail.com>
2017-12-29 23:30:32 -0500

    a regular commit

--- src/frontend/config/enviroment/stage.js
+++ src/frontend/config/enviroment/stage.js
@@ -2,7 +2,7 @@
  * @Author: Homer
  * @Date:   2017-12-29 17:47:30
  * @Last Modified by:   Homer
- * @Last Modified time: 2017-12-29 22:01:47
+ * @Last Modified time: 2017-12-29 23:30:22
  */
 'use strict';
 
@@ -23,10 +23,10 @@ module.exports = {
 	ip: process.env.IP || '0.0.0.0',
 
 	// API URL to be used in the client-side code
-	clientUrl: process.env.API_CLIENT_URL || `http://api.stage.reflen.com`,
+	clientUrl: process.env.API_CLIENT_URL || `http://console-api.stage.reflen.com`,
 	// API URL to be used in the server-side code
-	serverUrl: process.env.API_SERVER_URL || `http://api.stage.reflen.com}`,
+	serverUrl: process.env.API_SERVER_URL || `http://console-api.stage.reflen.com}`,
 	// API URL for graphql subscription ws
-	clientSubscriptionUrl: process.env.API_SUBSCRIPTION_URL || `ws://api.stage.reflen.com`
+	clientSubscriptionUrl: process.env.API_SUBSCRIPTION_URL || `ws://console-api.stage.reflen.com`
 
 };
